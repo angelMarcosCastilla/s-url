5be66eb94213b2e7306cd0932edb29508bb6e4c4
angelMarcosCastilla <ajmarcosc15@gmail.com>
2023-07-12 10:29:46 -0500

    fix rename dynamic

--- src/app/api/prueba/route.ts
+++ src/app/api/prueba/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-export const dinamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 export async function GET () {
   await new Promise(resolve => setTimeout(resolve, 10000))
   return NextResponse.json({ body: { message: 'Hello World 10min' } })
